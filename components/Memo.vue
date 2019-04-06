13654972d68247f727dc6a54682fd92df554a9c9
makoto <1988makoto0901@gmail.com>
2019-04-06 18:32:55 +0900

    新規作成

---
+++ components/Memo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    :style="{
+      left: `${posX}px`
+    }"
+    class="memo">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    posX: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.memo {
+  position: fixed;
+  top: 20px;
+  width: 200px;
+  height: 300px;
+  background: #ff0;
+}
+</style>
